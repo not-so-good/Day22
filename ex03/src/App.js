ddff68d11d98ddf223024ab14cd82ff8af5c1f19
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-29 13:32:24 +0200

    ex03 Create a Plant class component

---
+++ ex03/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './Plant.js'
+import Plant from './Plant.js';
+
+function App() {
+  return (
+    <div className="App">
+     <Plant />
+    </div>
+  );
+}
+
+export default App;
---
+++ ex03/src/Plant.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Plant extends Component {
+  render() {
+    return (
+      <div className="Plant">
+        <h2>This is a Plant Component!</h2>
+      </div>
+    );
+  }
+}
+
+export default Plant;
