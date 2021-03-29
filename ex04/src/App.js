3293fe1cc9b24b0dcb60cc0daa82b6afdad83859
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-29 14:02:01 +0200

    ex04 Create a Box class component

---
+++ ex04/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './Box.js'
+import Box from "./Box.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Box />
+    </div>
+  );
+}
+
+export default App;
---
+++ ex04/src/Box.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Box extends Component {
+  render() {
+    return (
+    <div className="Box">
+        This is a Box Component!
+        </div>
+    )
+  }
+}
+
+export default Box;
