24a05ca9545d86665f9b67af65abeda029f1f0fa
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-29 11:49:03 +0200

    ex01 Components

---
+++ ex01/src/App.js
@@ -0,0 +1,14 @@
+//src/App.js
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+          <div className="App">
+            <h1>Hello, React!</h1>
+          </div>
+        )
+    }
+}
+
+export default App;
